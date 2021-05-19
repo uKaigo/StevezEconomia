32ab1721ac731e5bd0587fe1aa1477cf39eca9e7
uKaigo <37600692+uKaigo@users.noreply.github.com>
2021-05-18 23:59:53 -0300

    feat: adicionar coinflip

---
+++ src/commands/fun/coinflip.ts
@@ -0,0 +1,35 @@
+import { Command } from 'discord-akairo'
+import { Message } from 'discord.js'
+
+interface CoinflipArgs {
+  choice: string
+}
+
+export default class CoinflipCommand extends Command {
+  constructor () {
+    super('coinflip', {
+      aliases: ['coinflip'],
+      args: [
+        {
+          id: 'choice',
+          type: ['cara', 'coroa'],
+          prompt: {
+            start: 'Você deseja ser `cara` ou `coroa`?',
+            retry: 'Escolha entre `cara` e `coroa`.'
+          }
+        }
+      ]
+    })
+  }
+
+  exec (message: Message, args: CoinflipArgs) {
+    const result = Math.round(Math.random())
+    const winner = result === 0 ? 'cara' : 'coroa'
+
+    if (args.choice === winner) {
+      return message.channel.send('Você ganhou!')
+    } else {
+      return message.channel.send('Eu ganhei!')
+    }
+  }
+}
