09d2d874424aa623723f9366d63358d0532db1b7
uKaigo <37600692+uKaigo@users.noreply.github.com>
2021-05-18 23:55:16 -0300

    feat(logs): mostrar o horario atual e adicionar Logger.debug

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,30 @@
-import { blue } from 'chalk'
+import { blueBright, gray, dim } from 'chalk'
 
 export class Logger {
+  lastDate: string | null
+  constructor () {
+    this.lastDate = null
+  }
+
+  private getDate (): string {
+    const date = new Date()
+    const hours = String(date.getHours()).padStart(2, '0')
+    const minutes = String(date.getMinutes()).padStart(2, '0')
+    const seconds = String(date.getSeconds()).padStart(2, '0')
+    const dateString = `${hours}:${minutes}:${seconds}`
+
+    if (this.lastDate === dateString) {
+      return ' '.repeat(this.lastDate.length + 2)
+    }
+    this.lastDate = dateString
+
+    return dim(gray(`[${dateString}]`))
+  }
+
+  debug (message: string) {
+    console.debug(`${this.getDate()} ${dim('[DEBUG]')} ${gray(message)}`)
+  }
   info (message: string) {
-    console.info(`${blue('[INFO]')} ${message}`)
+    console.info(`${this.getDate()} ${blueBright('[INFO]')}  ${message}`)
   }
 }
