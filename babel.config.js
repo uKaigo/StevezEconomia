10b60630c0be2f03320b63644b977687e6ad6a7a
uKaigo <37600692+uKaigo@users.noreply.github.com>
2021-05-28 19:01:12 -0300

    fix(babel): configurar allowDeclareFields

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: '14' }, modules: 'commonjs' }],
-    '@babel/preset-typescript'
+    ['@babel/preset-typescript', { allowDeclareFields: true }]
   ],
   plugins: [
     [
