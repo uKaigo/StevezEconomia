ef03058fd80b3a38b3152af52fe6602caaae7c73
uKaigo <37600692+uKaigo@users.noreply.github.com>
2021-05-27 21:39:18 -0300

    feat: integrar database

--- src/bot.ts
+++ src/bot.ts
@@ -2,11 +2,13 @@ import { AkairoClient, CommandHandler, ListenerHandler } from 'discord-akairo'
 import { join } from 'path'
 import { owners, prefix } from './config'
 import { Logger } from './utils/logger'
+import mongoose from 'mongoose'
 
 export class StevezBot extends AkairoClient {
   listenerHandler: ListenerHandler
   commandHandler: CommandHandler
   logger: Logger
+  mongoConnection: mongoose.Connection
 
   constructor () {
     super(
@@ -44,5 +46,34 @@ export class StevezBot extends AkairoClient {
 
     this.listenerHandler.loadAll()
     this.commandHandler.loadAll()
+
+    // Database
+    this.mongoConnection = mongoose.connection
+
+    this.registerDbEvents()
+
+    this.startDatabase()
+  }
+
+  private async registerDbEvents () {
+    this.mongoConnection.once('open', () => {
+      this.logger.info('Database conectada.')
+    })
+
+    this.mongoConnection.on(
+      'error',
+      this.logger.error.bind(this.logger, 'Erro no database:\n')
+    )
+  }
+
+  private async startDatabase () {
+    try {
+      await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+    } catch (e) {
+      this.logger.error(`Falha ao conectar ao MongoDB: ${e}`)
+    }
   }
 }
