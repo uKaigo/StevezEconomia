8694a62b2411ad5911d2710eb8520f33fcfbbdbc
uKaigo <37600692+uKaigo@users.noreply.github.com>
2021-06-11 23:04:17 -0300

    perf: configurar dotenv e debug somente em dev

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import { config as config_dotenv } from 'dotenv'
-import { set as mongoose_set } from 'mongoose'
 import { StevezBot } from './bot'
 
 if (process.env.NODE_ENV !== 'production') {
-  mongoose_set('debug', true)
+  require('dotenv').config({ debug: true })
+  require('mongoose').set('debug', true)
 }
-config_dotenv({ debug: process.env.NODE_ENV !== 'production' })
 
 const client = new StevezBot()
 client.login(process.env.DISCORD_TOKEN)
