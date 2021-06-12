39be0ded72b07c611e009f03960c04b9b68748b9
uKaigo <37600692+uKaigo@users.noreply.github.com>
2021-06-11 21:52:46 -0300

    feat: habilitar debug do mongoose quando fora de production

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { config as config_dotenv } from 'dotenv'
+import { set as mongoose_set } from 'mongoose'
 import { StevezBot } from './bot'
 
+if (process.env.NODE_ENV !== 'production') {
+  mongoose_set('debug', true)
+}
 config_dotenv({ debug: process.env.NODE_ENV !== 'production' })
 
 const client = new StevezBot()
