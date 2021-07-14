a85e29d824d2555841ce588216292f4d28089c57
uKaigo <37600692+uKaigo@users.noreply.github.com>
2021-07-13 23:04:06 -0300

    refactor: remover declaraçao do .env

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,12 +1,1 @@
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      DISCORD_TOKEN: string
-      MONGO_URI: string
-    }
-  }
-}
-
 declare module '@aero/centra'
-
-export {}
