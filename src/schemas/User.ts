6ceb78f8715f28fdc122aaaf41445ab7e676f3a2
uKaigo <37600692+uKaigo@users.noreply.github.com>
2021-07-20 20:14:52 -0300

    refactor(userschema): remover field "items"
    Nao e assim que a loja funcionara

--- src/schemas/User.ts
+++ src/schemas/User.ts
@@ -3,7 +3,6 @@ import { model, Schema } from 'mongoose'
 interface User {
   _id: string
   balance: number
-  items: number
   voiceXpAccumulator: number
   lastVoiceXp: number
   gamesPlayed: number
@@ -14,7 +13,6 @@ interface User {
 const userSchema = new Schema<User>({
   _id: { type: String, required: true },
   balance: { type: Number, default: 1000 },
-  items: { type: Number, default: 0 },
   voiceXpAccumulator: { type: Number, default: 0 },
   lastVoiceXp: { type: Number, default: 0 },
   gamesPlayed: { type: Number, default: 0 },
